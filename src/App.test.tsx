640f6747de82a67c63dcbb20b03d5449fb717c3a
Iryna Kvachuk <kvachukir@gmail.com>
2023-02-16 21:04:24 +0200

    BrowserRouter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { render, waitFor, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import store from './store';
 
-test('renders main', () => {
-  render(<App />);
-  const linkElement = screen.getByTestId('main');
-  expect(linkElement).toBeInTheDocument();
+test('renders main', async () => {
+  render(
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  );
+
+  await waitFor(async () => {
+    expect(screen.getByTestId('main')).toBeInTheDocument();
+  });
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+const rootElement = document.getElementById('root')!;
+const root = createRoot(rootElement);
 
 root.render(
-  <BrowserRouter>
+  <Router>
     <Provider store={store}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </Provider>
-  </BrowserRouter>
+  </Router>
 );

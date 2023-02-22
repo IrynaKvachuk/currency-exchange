4c14d2eb800bebd2639dcd8d7cccd53b158e6183
Iryna Kvachuk <kvachukir@gmail.com>
2023-02-22 14:06:57 +0200

    Header

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,15 +1,16 @@
+import { Link } from 'react-router-dom';
 import Container from '../Container';
 
 const Header: React.FC = () => {
   return (
     <Container>
-      <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-        <a
-          href="/"
-          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+      <header className="exchange_header d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <Link
+          to="/"
+          className="exchange_header-logo d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
         >
           Currency Exchange
-        </a>
+        </Link>
       </header>
     </Container>
   );

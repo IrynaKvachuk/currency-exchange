6470637aa587e9f4aebf108e0afac0d385334597
Iryna Kvachuk <kvachukir@gmail.com>
2023-02-22 21:22:10 +0200

    HashRouter for git pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 
 const rootElement = document.getElementById('root')!;
 const root = createRoot(rootElement);
 
 root.render(
-  <Router>
+  <HashRouter>
     <Provider store={store}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </Provider>
-  </Router>
+  </HashRouter>
 );
